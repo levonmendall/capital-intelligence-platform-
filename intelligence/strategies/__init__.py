bd096e74746beddb5066794c5da4b81582e1ec0a
levonmendall <levon_mendall@yahoo.com>
2026-07-22 21:17:07 -0700

    Create __init__.py

---
+++ intelligence/strategies/__init__.py
@@ -0,0 +1,9 @@
+"""Economic forecast strategy implementations."""
+
+from intelligence.strategies.rule_based import (
+    RuleBasedForecastStrategy,
+)
+
+__all__ = [
+    "RuleBasedForecastStrategy",
+]
