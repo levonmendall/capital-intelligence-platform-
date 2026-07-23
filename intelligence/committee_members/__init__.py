f989fec526d646b9fc209cf97e7db85e935a5011
levonmendall <levon_mendall@yahoo.com>
2026-07-23 11:20:24 -0700

    Update __init__.py

--- intelligence/committee_members/__init__.py
+++ intelligence/committee_members/__init__.py
@@ -1,7 +1,9 @@
 """Specialized investment committee members."""
 
 from intelligence.committee_members.macro import MacroCommitteeMember
+from intelligence.committee_members.risk import RiskCommitteeMember
 
 __all__ = [
     "MacroCommitteeMember",
+    "RiskCommitteeMember",
 ]
