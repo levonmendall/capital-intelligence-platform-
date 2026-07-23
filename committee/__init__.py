ae6607ef00a6b1f5e3111942e30208c24d53ab7a
levonmendall <levon_mendall@yahoo.com>
2026-07-22 18:29:21 -0700

    Create __init__.py

---
+++ committee/__init__.py
@@ -0,0 +1,12 @@
+from committee.consensus import CommitteeConsensus
+from committee.meeting import CommitteeMeeting, InvestmentCommittee
+from committee.member import CommitteeMember
+from committee.opinion import CommitteeOpinion
+
+__all__ = [
+    "CommitteeConsensus",
+    "CommitteeMeeting",
+    "CommitteeMember",
+    "CommitteeOpinion",
+    "InvestmentCommittee",
+]
